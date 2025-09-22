c6466d2f8c503c02ee86dcf0b500ba60cd58b396
Eduardo Teixeira <eteixeira566@gmail.com>
2025-09-22 19:55:44 -0300

    Criando a classe pessoa
    Criando a classe pessoa para realizar a instanciação posterior.

---
+++ WpfTesteTecnico/Models/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace WpfTesteTecnico.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Cpf { get; set; }
+        public string Endereco { get; set; }
+
+    }
+}
