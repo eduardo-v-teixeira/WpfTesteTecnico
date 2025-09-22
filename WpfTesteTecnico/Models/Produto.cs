398e7b914db411265aa0abaf413c3abce4bfbfb7
Eduardo Teixeira <eteixeira566@gmail.com>
2025-09-22 20:23:42 -0300

    Commit - Classe Produto
     Foi realizado a criação da classe e seu construtor

---
+++ WpfTesteTecnico/Models/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTesteTecnico.Models
+{
+    public class Produto
+    {
+        public Guid Id { get; private set; }
+        public string ?Nome { get; set; }
+        public int Codigo { get; set; }
+        public double Valor { get; set; }
+
+        public Produto()
+        {
+            //criando um id unico para cada produto e de maneira automatica
+            Id = Guid.NewGuid();
+        }
+        public Produto(string nome, int codigo, double valor)
+        {
+            Nome = nome;
+            Codigo = codigo;
+            Valor = valor;
+        }
+    }
+}
