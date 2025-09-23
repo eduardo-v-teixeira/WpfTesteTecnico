64c29c983dd5444e047797c8a881e480034df453
Eduardo Teixeira <eteixeira566@gmail.com>
2025-09-22 22:12:17 -0300

    Commit ClassePedidos
    Criando a classe pedidos

---
+++ WpfTesteTecnico/Models/Pedido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTesteTecnico.Models
+{
+    public class Pedido
+    {
+        public Guid Id { get; set; }
+        public Pessoa Pessoa { get; set; }
+        public List<Produto> Produtos { get; set; }
+        public decimal ValorTotal { get; set; }
+        public DateTime DataVenda { get; set; }
+
+    }
+}
