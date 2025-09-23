982f929c5258c2503919e738de5ad5e49450b1a7
Eduardo Teixeira <eteixeira566@gmail.com>
2025-09-22 22:11:51 -0300

    Commit Criando as Classes Enums

---
+++ WpfTesteTecnico/Models/Enums/FormaPagamento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTesteTecnico.Models.Enums
+{
+    public class FormaPagamento
+    {
+        public enum TipoPagamento
+        {
+            Dinheiro,
+            CartaoCredito,
+            Boleto,
+        }
+    }
+}
